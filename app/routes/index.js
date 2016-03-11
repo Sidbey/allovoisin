e98311e673fb8241e3b819bee38d12ccb977d2fb
Seelkare <seelkare@localhost.localdomain>
2016-03-11 16:42:38 +0100

    Maj de l'index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
+router.get('/connexion', function (req, res, next) {
+    res.render('connexion', {title: 'Express'});
+});
+
+router.get('/liste_offre', function (req, res, next) {
+    res.render('liste_offre', {title: 'Express'});
+});
+
+router.get('/proposition_offre', function (req, res, next) {
+    res.render('proposition_offre', {title: 'Express'});
+});
+
+router.get('/profil', function (req, res, next) {
+    res.render('profil', {title: 'Express'});
+});
+
+
 module.exports = router;
